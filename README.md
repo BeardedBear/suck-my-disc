64f825300c282a26022cea9948e4eefbb3c6b4e1
Germain <germain.poirrier@gmail.com>
2019-04-24 11:30:54 +0200

    Test API.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# my-project
+# Vue Learning
 
 ## Project setup
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,11 +3,13 @@
     <h1>{{ msg }}</h1>
     <p>{{ get_count }}</p>
     <button @click="act_multiplier()">Multiplier</button>
+    <button @click="getGreetings()">Login</button>
   </div>
 </template>
 
 <script>
 import Vuex from "vuex";
+// import { HTTP } from "../api";
 
 export default {
   name: "HelloWorld",
@@ -15,7 +17,42 @@ export default {
     msg: { type: String, default: "non" }
   },
   methods: {
-    ...Vuex.mapActions(["act_multiplier"])
+    ...Vuex.mapActions(["act_multiplier"]),
+    // getGreetings: function() {
+    //   HTTP.post("/api/v4/users/login", {
+    //     login_id: "g.poirrier@allo-media.fr",
+    //     password: "1RUT%6!8JHL*$q1Roa4&7dRX"
+    //   })
+    //     .then(response => {
+    //       this.greetings = response.message;
+    //     })
+    //     .catch(e => {
+    //       this.errors = e;
+    //     });
+    // },
+
+    // "POST" "/api/v4/users/login HTTP/1.1"
+    // "Host": "chat.allo-media.net"
+    // "Accept": "*"
+    // "User-Agent": "Mozilla/5.0 (compatible; Rigor/1.0.0; http://rigor.com)"
+    // "Content-Length": "77"
+    // Content-Type: application/x-www-form-urlencoded
+
+    getGreetings: function() {
+      fetch("https://chat.allo-media.net/api/v4/users/login", {
+        method: "post",
+        body: JSON.stringify({
+          login_id: "g.poirrier@allo-media.fr",
+          password: "1RUT%6!8JHL*$q1Roa4&7dRX"
+        })
+      })
+        .then(response => {
+          this.greetings = response.message;
+        })
+        .catch(e => {
+          this.errors = e;
+        });
+    }
   },
   computed: {
     ...Vuex.mapGetters(["get_count"])
@@ -23,7 +60,6 @@ export default {
 };
 </script> 
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 h3 {
   margin: 40px 0 0;
